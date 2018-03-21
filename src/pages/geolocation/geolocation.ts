ea201f516ff75189db3a406f5e04ec128b84b45f
woosuk-oh <gta2v@naver.com>
2018-03-21 11:48:19 +0900

    GeoLocation List형태로 출력

--- src/pages/geolocation/geolocation.ts
+++ src/pages/geolocation/geolocation.ts
@@ -1,112 +1,117 @@
-import { Component } from '@angular/core';
-import { NavParams, LoadingController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+import {Component} from '@angular/core';
+import {NavParams, LoadingController} from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
 
 @Component({
-    templateUrl:'latlong.html'
+  templateUrl: 'latlong.html'
 })
 
 export class latlongPage {
 
 
-    lat: any;
-    lng: any;
-
-    items: Array<{lat: string, lng: string}>;
+  items: Array<{ lat: any, lng: any}>;
 
 
-    loading: any;
+  loading: any;
 
-    constructor(
-        public geolocation: Geolocation,
-        public loadingCtrl: LoadingController
+  constructor(public geolocation: Geolocation,
+              public loadingCtrl: LoadingController) {
+    this.loading = this.loadingCtrl.create();
+    this.items = [];
 
-    ){
-        this.loading = this.loadingCtrl.create();
-        this.items = [];
-        
+  }
+
+
+  tryGeolocation() {
+    this.loading.present();
+
+    let onSuccess = (postion) => {
+      alert(
+        'Lat' + postion.coords.latitude + '\n' +
+        'Long' + postion.coords.longitude + '\n' +
+        'Accuracy' + postion.coords.accuracy + '\n'
+      );
+      this.loading.dismiss();
+
+    };
+
+    let onError = (error) => {
+      alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n'
+      );
+      this.loading.dismiss();
     }
 
 
-    tryGeolocation(){
-        this.loading.present();
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-        let onSuccess = (postion) => {
-            alert(
-                'Lat'       + postion.coords.latitude + '\n' +
-                'Long'      + postion.coords.longitude + '\n'+
-                'Accuracy'  + postion.coords.accuracy + '\n' 
-            );
-            this.loading.dismiss();
-        
-        };
+    // this.geolocation.getCurrentPosition().then((resp) => {
+    //     let pos = {
+    //         lat: resp.coords.latitude,
+    //         lng: resp.coords.longitude
+    //     };
 
-        let onError = (error) => {
-            alert('code: ' +error.code + '\n' +
-                   'message: ' + error.message + '\n'     
-            );
-            this.loading.dismiss();
-        }
-
-        
-            
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-        // this.geolocation.getCurrentPosition().then((resp) => {
-        //     let pos = {
-        //         lat: resp.coords.latitude,
-        //         lng: resp.coords.longitude
-        //     };
-
-        //     this.loading.dismiss();
-        // }).catch((error) => {
-        //     console.log('Error Getting Location ', error);
-        //     this.loading.dismiss();
-        // })
+    //     this.loading.dismiss();
+    // }).catch((error) => {
+    //     console.log('Error Getting Location ', error);
+    //     this.loading.dismiss();
+    // })
 
 
-    }
-    watchGeolocation(){
-        this.loading.present();
+  }
 
-       
-        // let wt = this.geolocation.watchPosition();
-        
-        // wt.subscribe((data) => {
-        //     let pos = {
-        //         lat: data.coords.latitude,
-        //         lng: data.coords.longitude
-        //     }
-        // })
+  watchGeolocation() {
+    this.loading.present();
 
-        // wt.subscribe()
 
-        // this.items.push()
-        
-        let onSuccess = (postion) => {
-            // alert(
-            //     'Lat'       + postion.coords.latitude + '\n' +
-            //     'Long'      + postion.coords.longitude + '\n'+
-            //     'Accuracy'  + postion.coords.accuracy + '\n' 
-            // );
-                        
-            this.lat = postion.coords.latitude;
-            this.lng = postion.coords.longitude;
+    // let wt = this.geolocation.watchPosition();
 
-            this.loading.dismiss();
-        
-        };
+    // wt.subscribe((data) => {
+    //     let pos = {
+    //         lat: data.coords.latitude,
+    //         lng: data.coords.longitude
+    //     }
+    // })
 
-        let onError = (error) => {
-            alert('code: ' +error.code + '\n' +
-                   'message: ' + error.message + '\n'     
-            );
-            this.loading.dismiss();
-        }
-        return navigator.geolocation.watchPosition(onSuccess, onError, {enableHighAccuracy: true, timeout: 30000, maximumAge: 3000})
+    // wt.subscribe()
+
+    // this.items.push()
+
+    let onSuccess = (postion) => {
+      // alert(
+      //     'Lat'       + postion.coords.latitude + '\n' +
+      //     'Long'      + postion.coords.longitude + '\n'+
+      //     'Accuracy'  + postion.coords.accuracy + '\n'
+      // );
+
+
+      this.items.push({
+        lat: postion.coords.latitude,
+        lng: postion.coords.longitude
+      });
+
+      console.log(postion.coords.latitude)
+
+      this.loading.dismiss();
+
+    };
+
+    let onError = (error) => {
+      alert('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n'
+      );
+      this.loading.dismiss();
     }
 
-    
+
+    return navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+      timeout: 30000,
+      maximumAge: 3000
+    })
+  }
+
+
 }
 
 
