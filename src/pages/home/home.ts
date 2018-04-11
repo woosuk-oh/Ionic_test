1c56782744c63063b2b5a2c75c8524bfd393c5c1
woosuk-oh <gta2v@naver.com>
2018-04-11 18:04:40 +0900

    1. bluetooth Test - 완료 2. Camera - 카메라 켜서 사진 찍고 불러오기, 앨범에서 사진 가져오고 불러오기

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
-import {AlertController, NavController, Platform} from 'ionic-angular';
+import {AlertController, NavController, Platform, ToastController} from 'ionic-angular';
 import {Device} from '@ionic-native/device';
 import {Geolocation} from '@ionic-native/geolocation';
 import {BatteryStatus} from "@ionic-native/battery-status";
 import {BluetoothSerial} from "@ionic-native/bluetooth-serial";
+import {DomSanitizer} from '@angular/platform-browser';
 import {Camera, CameraOptions, CameraPopoverOptions} from "@ionic-native/camera";
 import Util from './util'
 
@@ -26,17 +27,29 @@ export class HomePage {
   currentSSID: any;
   batteryPercent: any;
 
-  constructor(public navCtrl: NavController,
-              private platform: Platform,
-              private device: Device,
-              private geolocation: Geolocation,
-              private batteryStatus: BatteryStatus,
-              private bluetoothSerial: BluetoothSerial,
-              public alertCtrl: AlertController,
-              private camera: Camera) {
+  public photos: any;
+  public base64Image: string;
+  public processString: string;
+  private urlPrefix: string = 'file://';
+  private base64Prefix: string = 'data:image/jpeg;base64,';
+  private sendImage: string;
+  private imageblob: any;
+
+  constructor(
+    public domSanitizer: DomSanitizer,
+    public navCtrl: NavController,
+    private platform: Platform,
+    private device: Device,
+    private geolocation: Geolocation,
+    private batteryStatus: BatteryStatus,
+    private bluetoothSerial: BluetoothSerial,
+    public alertCtrl: AlertController,
+    private camera: Camera,
+    private toastCtrl: ToastController,
+  ) {
 
 
-    this.currentSSID= [];
+    this.currentSSID = [];
 
     this.delay = 2000; // delay in ms for timeout
 
@@ -54,7 +67,13 @@ export class HomePage {
     })
   }
 
-  ionViewDidEnter() {}
+  ionViewDidEnter() {
+  }
+
+
+  ngOnInit() {
+    this.photos = [];
+  }
 
 
   timeout() {
@@ -64,44 +83,139 @@ export class HomePage {
     });
   }
 
+  deletePhoto(index) {
+    let confirm = this.alertCtrl.create({
+      title: 'Sure you want to delete this photo? There is NO undo!',
+      message: '',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            console.log('Agree clicked');
+            this.photos.splice(index, 1);
+            //return true;
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 
-  cameraPic(){
-
+  choosePhoto() {
     const options: CameraOptions = {
-      quality: 100,
+      quality: 50,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: true,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
-      saveToPhotoAlbum: true,
+      saveToPhotoAlbum: false
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      // const safeUrl: any = this.domSanitizer.bypassSecurityTrustUrl(this.base64Prefix + imageData);
+      const safeUrl: any = this.base64Prefix + imageData;
+      this.sendImage = safeUrl;
+      this.photos.push(safeUrl);
+      this.photos.reverse();
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      // destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(options).then(
+      (imageData) => {
+        // this.base64Image = this.urlPrefix + imageData;
+        const safeUrl: any = this.base64Prefix + imageData;
+        this.photos.push(safeUrl);
+        this.photos.reverse();
+      },
+      (err) => {
+        console.log(err);
+      });
+  }
+
+  uploadPhoto() {/*
+    console.log('uploadPhoto click!');
+    let storageRef = firebase.storage().ref();
+    // let storageRef = firebase.storage().refFromURL('gs://moblile-base.appspot.com');
+    this.processString = '';
+    this.processString += 'get storage!';
+    // this.showToast('get storage!');
+    // Create a timestamp as filename
+    const filename = Math.floor(Date.now() / 1000);
+    console.log('storageRef =>', storageRef.name);
+    // Create a reference to 'images/todays-date.jpg'
+    const imagesRef = storageRef.child('images');
+    console.log('imagesRef =>', imagesRef);
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+    // this.showToast('Create a reference!');
+    this.processString += 'Create a reference!';
+    console.log('Create a reference!');
+    const metadata = {
+      contentType: 'image/jpeg',
+    };
+    // imageRef.put(this.b64toBlob(this.imageblob, 'image/jpeg'), metadata)
+    imageRef.putString(this.sendImage, firebase.storage.StringFormat.DATA_URL).then(
+      (snapshot: any) => {
+        // this.showToast('upload success!');
+        this.processString += 'upload success!';
+      },
+      (err) => {
+        console.log(err);
+        this.processString += 'upload fail => ' + JSON.stringify(err);
+        // this.showToast('upload fail => '+ JSON.stringify(err));
+      });
+  */
+  }
+
+  private b64toBlob(b64Data, contentType, sliceSize?) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    let byteCharacters = atob(b64Data);
+    let byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      let slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      let byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      let byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
     }
 
-
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64:
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-      this.displayErrorAlert(err);
-    });
+    let blob = new Blob(byteArrays, {type: contentType});
+    return blob;
   }
 
-
-
-  displayErrorAlert(err){
-    console.log(err);
-    let alert = this.alertCtrl.create({
-      title: 'Error',
-      subTitle: 'Error while trying to capture picture',
-      buttons: ['OK']
+  private showToast(text: string) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 5000,
+      position: 'top'
     });
-    alert.present();
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
-
-
-  // openModal(){
-  //   util.prototype.callModal()
-  // }
-
-
 }
